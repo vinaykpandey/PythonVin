ff88fa68e0a2d35660959e93b255ffb985d46d49
vinay <vinay@vinays-MacBook-Pro.local>
2018-05-07 16:24:47 +0530

    filter, reduce

---
+++ Advance/filter.py
@@ -0,0 +1,12 @@
+'''
+As the name suggests, filter creates a list of elements for
+which a function returns on certain condition
+
+'''
+
+
+number_list = range(-5, 5)
+less_than_zero = list(filter(lambda x: x < 0, number_list))
+print(less_than_zero)
+
+# Output: [-5, -4, -3, -2, -1]
--- Advance/reduce.py
+++ Advance/reduce.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
 '''
-reduce(aFunction, aSequence)
+Reduce is a really useful function for performing some computation on a list and returning the result
 
+functool module
 '''
+
+from functools import reduce
+product = reduce((lambda x, y: x * y), [2, 3, 4,5,7])
+print(product)
+
+
