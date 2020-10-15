a1f87b3efe7029fd35cbb10f905356db7e8b7eca
Vinay Pandey <vinay.pandey12@gmail.com>
2020-10-15 10:35:05 +0530

    strings

---
+++ Base/Strings/info.py
@@ -0,0 +1,23 @@
+s1 = "vinay pandey"
+# print(s1)
+# for c in s1:
+#     print(c)
+# for i in range(len(s1)):
+#     print(s1[i])
+
+print(s1.index("n"))
+print(len(s1))
+print(s1.count("a"))
+print(s1[3:10]) # ay pand
+print(s1[3:7:2])
+#first character
+print('s1[0] = ', s1[0])
+#Accessing string characters in Python
+#first character
+print('str[0] = ', s1[0])
+#last character
+print('s1[-1] = ', s1[-1])
+#slicing 2nd to 5th character
+print('s1[1:5] = ', s1[1:5])
+#slicing 6th to 2nd last character
+print('s1[5:-2] = ', s1[5:-2])
