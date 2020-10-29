66f777caaf2c840d609cdce068a75a412d266253
Vinay Pandey <vinay.pandey12@gmail.com>
2020-10-29 19:15:46 +0530

    celery beat

--- Celery/periodic_task_v2.py
+++ Celery/periodic_task_v2.py
@@ -1,12 +1,14 @@
-from celery import Celery
+from celery import Celery, Task
 from celery.schedules import crontab
 from functions import file_write
+from celery.worker.request import Request
+
 
 BROKER_URL = 'redis://localhost:6379/0'
 BACKEND_URL = 'redis://localhost:6379/1'
 app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL)
 
-class Person:
+class Person(Task):
     def __init__(self):
         self.name = "Vinay Pandey"
 
@@ -30,19 +32,22 @@ def setup_periodic_tasks(**kwargs):
     # )
 
 
-@app.task
-def test(arg):
+@app.task(bind=True, base=Person)
+def test(self, arg):
     print("Celery beat is running----")
-    file_write("Celery beat is running----")
     print(arg)
+    print(self.__dict__)
 
 
+
+"""
+# celery worker -h
 # celery -A periodic_task_v2 beat --loglevel=info
 # celery -A periodic_task_v2 beat -l debug
 # celery -A periodic_task_v2 worker -B --loglevel=info  # case 1
 # celery -A periodic_task_v2 worker -l --loglevel=info
-"""
-n bash shell,when run command: celery beat -A proj -l info, only send to queue, not runing the task,
+
+In bash shell,when run command: celery beat -A proj -l info, only send to queue, not runing the task,
 but change the command celery worker -A proj -l info, the task will trigger.
 Beat does not execute tasks, it just sends the messages. You need both a beat instance and a worker instance!s
 For running periodic tasks you have to run two services: celery beat together with celery worker.
