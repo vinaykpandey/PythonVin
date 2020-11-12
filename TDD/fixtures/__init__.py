db14d281fb318883d87211f91cf81f6bab5751fc
Vinay Pandey <vinay.pandey12@gmail.com>
2020-11-12 22:37:35 +0530

    conftest fixtures

--- TDD/fixtures/__init__.py
+++ TDD/fixtures/__init__.py
@@ -10,4 +10,10 @@ A function is marked as a fixture by −
 
 pytest -v (for all test case)
 pytest -k divisible -v   (for samename testcase)
+
+However, the approach comes with its own limitation. A fixture function defined inside a test
+file has a scope within the test file only. We cannot use that fixture in another test file
+
+To make a fixture available to multiple test files, we have to define the fixture function in a file called conftest.py.
+
 """
