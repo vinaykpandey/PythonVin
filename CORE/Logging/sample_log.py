169e2d96498097d7b82f30f3cf8e811ab8df397d
Vinay Pandey <vinay.pandey12@gmail.com>
2020-12-07 23:05:09 +0530

    logging py

---
+++ CORE/Logging/sample_log.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import logging
+logging.debug('So should this')
+logging.info('And this, too')
+logging.warning('Watch out!')  # will print a message to the console (this is default level)
+logging.critical('I told you so')  #
+logging.error('I told you so')  #
